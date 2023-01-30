90cf2a0acacec096711176e87fecc2c45d8fac50
Joshua Pinter <joshuapinter@gmail.com>
2023-01-30 10:19:26 -0700

    Support `priority` in ActiveJob queue handler.
    We use ActiveJob but specify DelayedJob as the actual queue adapter via:
    
    `config.active_job.queue_adapter = :delayed_job`
    
    I’m not sure if other handlers use `priority`, but this is our fork and we need to support `priority` when passed in as an option.

--- lib/delayed_paperclip/jobs/active_job.rb
+++ lib/delayed_paperclip/jobs/active_job.rb
@@ -3,7 +3,8 @@ module DelayedPaperclip
     class ActiveJob < ActiveJob::Base
       def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
         queue_name = instance_klass.constantize.paperclip_definitions[attachment_name][:delayed][:queue]
-        set(:queue => queue_name).perform_later(instance_klass, instance_id, attachment_name.to_s)
+        priority   = instance_klass.constantize.paperclip_definitions[attachment_name][:delayed][:priority]
+        set( priority: priority, queue: queue_name).perform_later(instance_klass, instance_id, attachment_name.to_s)
       end
 
       def perform(instance_klass, instance_id, attachment_name)
